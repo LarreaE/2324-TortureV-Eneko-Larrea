e12cd9dde89ad7429c7aa3064ab4096c879dddd0
LarreaE <eneko.larrea@ikasle.aeg.eus>
2024-01-26 10:27:05 +0100

    [UPDATE]colisiones del player añadidas

--- src/gameLogic.js
+++ src/gameLogic.js
@@ -33,21 +33,41 @@ export default function update()
 
 function updatePlaying()
 {
+    console.log(globals.level[0].data[0][8]);
     if (globals.action.moveUp && globals.previousUp === false) {
-        console.log("moveup");
         globals.playerPos[0] -= 1;  //va hacia arriba
+
+        if (globals.level[0].data[globals.playerPos[0]][globals.playerPos[1]] === 4) { //hay colision
+            globals.playerPos[0] += 1; //ha habido colision, corregimos
+            console.log("colision");
+        }
     }
     else if (globals.action.moveDown && globals.previousDown === false) {
-        console.log("movedow");
         globals.playerPos[0] += 1;  //va hacia abajo
+
+        if (globals.level[0].data[globals.playerPos[0]][globals.playerPos[1]] === 4) { //hay colision
+            globals.playerPos[0] -= 1; //ha habido colision, corregimos
+            console.log("colision");
+
+        }
     }
     else if (globals.action.moveLeft && globals.previousLeft === false) {
-        console.log("moveulef");
         globals.playerPos[1] -= 1;  //va hacia la izquierda
+
+        if (globals.level[0].data[globals.playerPos[0]][globals.playerPos[1]] === 4) { //hay colision
+            globals.playerPos[1] += 1; //ha habido colision, corregimos
+            console.log("colision");
+
+        }
     }
     else if (globals.action.moveRight && globals.previousRight === false) {
-        console.log("moverigth");
         globals.playerPos[1] += 1;  //va hacia la derecha
+
+        if (globals.level[0].data[globals.playerPos[0]][globals.playerPos[1]] === 4) { //hay colision
+            globals.playerPos[1] -= 1; //ha habido colision, corregimos
+            console.log("colision");
+
+        }
     }
 
     globals.previousDown    = globals.action.moveDown;
@@ -55,4 +75,20 @@ function updatePlaying()
     globals.previousLeft    = globals.action.moveLeft;
     globals.previousRight   = globals.action.moveRight;
 
+}
+
+function updateTimer(timer)
+{
+     //incrementamos el contador de cambio de valor
+     timer.timeChangeCounter += globals.deltaTime;
+    
+     //si ha pasado el timepo necesario, cambiamos el valor del timer
+     if (timer.timeChangeCounter > timer.timeChangeValue) {
+         
+         timer.value--;
+ 
+         //reseteamos timeChangeCounter
+         timer.timeChangeCounter = 0;
+ 
+     }
 }
